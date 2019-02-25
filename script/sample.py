fb86ba002bd82497021720f59861c62a348fa706
nagaminenot <nagamine@slogan.jp>
2019-02-25 18:18:52 +0900

    twitterリストから自動フォロー

--- script/sample.py
+++ script/sample.py
@@ -4,31 +4,84 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from time import sleep
 import datetime
+from pprint import pprint
 
-def execSearch(browser: webdriver):
+USER_NAME = 'USER_NAME'
+PASSWORD = 'PASSWORD'
+LIST_NAME = 'media'
+TOTAL_FOLLOW_LIMIT = 20
+ACCOUNT_FOLLOW_LIMIT = 5
+
+def loginTwitter(browser: webdriver):
     """
-    Googleで検索を実行する
+    Twitterにログインする
     :param browser: webdriver
     """
     # スクリーンショットのファイル名用に日付を取得
     dt = datetime.datetime.today()
     dtstr = dt.strftime("%Y%m%d%H%M%S")
 
-    # Googleにアクセス
-    browser.get('https://www.google.co.jp/')
+    # twitterにアクセス
+    browser.get('https://twitter.com/login')
     sleep(1)
 
-   # キーワードの入力
-    search_box = browser.find_element_by_name("q")
-    search_box.send_keys('docker selenium')
-
-    # 検索実行
-    search_box.submit()
+   # ログイン情報の入力
+    username_or_email = browser.find_element_by_xpath("//*[@id='page-container']/div/div[1]/form/fieldset/div[1]/input")
+    username_or_email.send_keys(USER_NAME)
+    password = browser.find_element_by_xpath("//*[@id='page-container']/div/div[1]/form/fieldset/div[2]/input")
+    password.send_keys(PASSWORD)
+    # ログイン
+    password.submit()
     sleep(1)
 
-    # スクリーンショット
+def getAccountsFromList(browser: webdriver):
+    """
+    Twitterでリストのフォロワーを抽出する
+    :param browser: webdriver
+    """
+    # スクリーンショットのファイル名用に日付を取得
+    dt = datetime.datetime.today()
+    dtstr = dt.strftime("%Y%m%d%H%M%S")
+
+    # リストにアクセスする
+    browser.get('https://twitter.com/'+ USER_NAME +'/lists/'+ LIST_NAME +'/members')
     browser.save_screenshot('images/' + dtstr + '.png')
 
+    accounts = browser.find_elements_by_class_name("js-actionable-user")
+    account_list = []
+    for account in accounts:
+        account_list.append(account.get_attribute('data-screen-name'))
+    return account_list
+
+def getNotFollowingAccounts(browser: webdriver, account):
+    """
+    Twitterでアカウントのフォロワーをフォローする
+    :param browser: webdriver
+    """
+    # スクリーンショットのファイル名用に日付を取得
+    dt = datetime.datetime.today()
+    dtstr = dt.strftime("%Y%m%d%H%M%S")
+
+    # リストにアクセスする
+    browser.get('https://twitter.com/'+ account + '/followers')
+    grid = browser.find_element_by_class_name("GridTimeline")
+    profile_cards = grid.find_elements_by_class_name("js-actionable-user")
+    limit = ACCOUNT_FOLLOW_LIMIT
+    follow_count = 0
+    for profile_card in profile_cards:
+        if limit > 0:
+            is_follow = profile_card.find_elements_by_class_name("not-following")# ない
+            description = profile_card.find_element_by_class_name("ProfileCard-bio")
+            description_text = description.text
+            follow_btn = profile_card.find_elements_by_class_name("follow-text")
+            if len(description_text) > 20 and is_follow != [] and follow_btn != [] :
+                follow_btn[0].click()
+                sleep(1)
+                limit = limit - 1
+                follow_count = follow_count + 1
+        else:
+            break
+    return follow_count
 
 if __name__ == '__main__':
     try:
@@ -40,8 +93,18 @@ if __name__ == '__main__':
             command_executor='http://selenium-hub:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME)
 
-        # Googleで検索実行
-        execSearch(browser)
+        # Twitterにログイン
+        loginTwitter(browser)
+        # Twitterで自動フォローする
+        accounts = getAccountsFromList(browser)
+        targetAccounts = []
+        total_follow = TOTAL_FOLLOW_LIMIT
+        for account in accounts:
+            if account != None:
+                if total_follow > 0:
+                    total_follow - getNotFollowingAccounts(browser, account)
+                else:
+                    break
 
     finally:
         # 終了
